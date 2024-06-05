f4c6ae85a2a64dcb9faf03075cbf354516d72076
TomiRenaldi <tomirenaldi2@gmail.com>
2024-06-05 11:54:30 +0700

    :lipstick: Update background video & overlay

--- src/components/BackgroundVideos.jsx
+++ src/components/BackgroundVideos.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import videoSource from '../assets/vidbackground.mp4';
+import videoSource from '../assets/vid-01.mp4';
 
 const BackgroundVideo = () => {
     return (
@@ -12,12 +12,12 @@ const BackgroundVideo = () => {
                 muted
                 playsInline
             />
-            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
+            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-70"></div>
             <div className="relative z-10 flex items-center h-full px-4">
-                <div className="max-w-lg text-left">
-                    <h1 className="text-4xl font-bold text-white">Muara Toba Persada</h1>
-                    <p className="mt-4 text-white">
-                        Kami tuangkan semua kegiatan foto, video, dari semua kegiatan & acara berlibur disini, cobalah sedikit berpetualang lalu download bagian penting kalian. 
+                <div className="max-w-lg text-left md:ml-8 lg:ml-16">
+                    <h1 className="text-4xl lg:text-4xl font-bold text-[#0A51B2]">Muara Toba Persada Holiday.</h1>
+                    <p className="mt-4 text-white text-base md:text-lg lg:text-xl">
+                        Momen bersenang - senang melepas penat, mencari kebahagiaan bersama - sama, berlibur dengan seluruh jajaran karyawan dan manajemen pengelola.
                     </p>
                 </div>
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from '../assets/BRI White.png'; // Ganti dengan path logo Anda
+import logo from '../assets/logona.png'; // Ganti dengan path logo Anda
 
 const Navbar = () => {
     return (
-        <nav className="fixed top-9 left-9 w-full bg-transparent p-4 z-50">
+        <nav className="fixed left-5 md:left-12 w-full bg-transparent z-50">
             <div className="container mx-auto flex items-center">
-                <img src={logo} alt="Logo" className="w-12 h-10 md:w-12 md:h-5" />
+                <img src={logo} alt="Logo" className="w-40 h-40 md:w-40 md:h-40" />
             </div>
         </nav>
     );
