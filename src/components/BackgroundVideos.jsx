99c381fe0842f30b3502c6f70880dd982fcaa433
TomiRenaldi <tomirenaldi2@gmail.com>
2024-06-05 13:28:26 +0700

    :sparkles: Add download button

--- src/components/BackgroundVideos.jsx
+++ src/components/BackgroundVideos.jsx
@@ -12,13 +12,20 @@ const BackgroundVideo = () => {
                 muted
                 playsInline
             />
-            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-70"></div>
+            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-80"></div>
             <div className="relative z-10 flex items-center h-full px-4">
                 <div className="max-w-lg text-left md:ml-8 lg:ml-16">
                     <h1 className="text-4xl lg:text-4xl font-bold text-[#0A51B2]">Muara Toba Persada Holiday.</h1>
                     <p className="mt-4 text-white text-base md:text-lg lg:text-xl">
                         Momen bersenang - senang melepas penat, mencari kebahagiaan bersama - sama, berlibur dengan seluruh jajaran karyawan dan manajemen pengelola.
                     </p>
+                    <a
+                        href={videoSource}
+                        download="video.mp4"
+                        className="mt-7 inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
+                    >
+                        Download Video
+                    </a>
                 </div>
             </div>
         </div>
